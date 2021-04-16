a3bd725aaabcae4e367bd67b043d45cf025560d2
shahid ali <shahidali23032001@gmail.com>
2021-04-17 03:06:49 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import CarItem from "./components/CarItem";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.carContainer}>
+        <ImageBackground
+          source={require("./assets/images/ModelS.jpeg")}
+          style={styles.image}
+        />
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69,420</Text>
+        </View>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  subtitle: {
+    fontSize: 14,
+    color: "#5c5e62",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+});
